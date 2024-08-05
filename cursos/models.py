c99c6e256091362e71d7e33be6a666c61b8c309b
JOÃO OTAVIO PESSOA HENRIQUE <j.otavio@academico.ifrn.edu.br>
2024-08-05 08:19:45 -0300

    treino em sala

--- cursos/models.py
+++ cursos/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+#fiz em sala 
+def Modalidade(models.Model):
+    nome= models.Charfield(max_length=150)
+
+    def __str__(self):
+        return self.nome
+
+def Curso(models.Model):
+    nome= models.Charfield(max_length=150)
+
+    def __str__(self):
+        return self.nome
--- cursos/views.py
+++ cursos/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import Curso,Modalidade
 # Create your views here.
-
+# fiz em sala 
 def index(request):
+    lista_cursos= Curso.objects.all()
+    lista_modalidade=Modalidade.objects.all()
+    context ={
+        'cursos': lista_cursos
+        'modalidades': lista_modalidade
+    }
     return render(request, 'cursos/index.html')
 
 def ads(request):
